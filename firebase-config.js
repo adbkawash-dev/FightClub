6f0b2ff8d9a68bd2b052cfd80ff2e24c614fa4ef
BetterCallDrizzy <adb.kawash@gmail.com>
2026-05-05 20:58:09 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,9 @@
-
+window.SFC_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDqa9i-yVKWqEYMPe-cpv_yHbNoLjzXsAU",
+  authDomain: "school-fight-club.firebaseapp.com",
+  projectId: "school-fight-club",
+  storageBucket: "school-fight-club.firebasestorage.app",
+  messagingSenderId: "535299587856",
+  appId: "1:535299587856:web:c262d52391b56ef1812f95",
+  measurementId: "G-G0LM348GTL"
+};
